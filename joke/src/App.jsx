1551df089be609a22e6f4fa6ff2799f493fa5c3c
fazza-tech <ebranfazza@gmail.com>
2024-12-23 11:09:22 +0530

    updated

---
+++ joke/src/App.jsx
@@ -0,0 +1,25 @@
+import Joke from "./assets/Joke"
+
+export default function App(){
+  return(
+    <>
+      <Joke
+    
+        Punchline= "I can't wait to see her face light up when she opens it."
+      />
+      <Joke
+        Setup="How did the hacker escape the police?"
+        Punchline= "He just ransomware!"
+      />
+      <Joke
+        Setup="Why don't pirates travel on mountain roads?"
+        Punchline="Scurvy"
+      />
+      <Joke
+        Setup=" What's the best thing about Switzerland?"
+        Punchline= "I don't know, but the flag is a big plus!"
+      />
+    </>
+    
+  )
+}
---
+++ joke/src/assets/Joke.jsx
@@ -0,0 +1,10 @@
+export default function Joke(props){
+    return(
+        <>
+         {props.Setup && <p className="setup">Setup: {props.Setup}</p>} 
+         <p className="punchline">Punchline: {props.Punchline}</p>
+         <hr />
+            
+        </>
+    )
+}
